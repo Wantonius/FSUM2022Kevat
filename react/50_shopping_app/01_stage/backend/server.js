076b6c49ff3d40404d491e205368f91064d18829
Erno Hentonen <erno.hentonen@opiframe.com>
2022-05-04 11:09:15 +0300

    stage 1 part 1

--- react/50_shopping_app/01_stage/backend/server.js
+++ react/50_shopping_app/01_stage/backend/server.js
@@ -19,6 +19,24 @@ app.get("/api/shopping",function(req,res) {
 	return res.status(200).json(database);
 });
 
+app.post("/api/shopping",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad request"});
+	}
+	if(!req.body.type) {
+		return res.status(400).json({message:"Bad request"});
+	}
+	let item = {
+		type:req.body.type,
+		count:req.body.count,
+		price:req.body.price,
+		id:id
+	}
+	id++;
+	database.push(item);
+	return res.status(201).json(item);
+})
+
 app.listen(port);
 
 console.log("Running in port ",port);
