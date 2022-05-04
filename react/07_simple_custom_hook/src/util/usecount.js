398809ae7cfa6dd1fb3a82020edba009f0734fec
Erno Hentonen <erno.hentonen@opiframe.com>
2022-05-04 10:13:42 +0300

    simple custom hook

---
+++ react/07_simple_custom_hook/src/util/usecount.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+export const useCount = (initialValue = 0) => {
+	
+	const [value,setValue] = useState(initialValue);
+	
+	const add = () => {
+		setValue(value => value+1);
+	}
+	
+	const substract = () => {
+		setValue(value => value-1);
+	}
+	
+	return [value,add,substract];
+}
+
