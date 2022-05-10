e9b46d8f4fd923d3287b222919422834b6c79bee
Erno Hentonen <erno.hentonen@opiframe.com>
2022-05-10 09:53:20 +0300

    login first step

---
+++ react/50_shopping_app/02_stage_login/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const apiroutes = require("./routes/apiroutes");
+
+let app = express();
+
+app.use(express.json());
+
+const port = process.env.port || 3001;
+
+app.use("/api",apiroutes);
+
+app.listen(port);
+
+console.log("Running in port ",port);
