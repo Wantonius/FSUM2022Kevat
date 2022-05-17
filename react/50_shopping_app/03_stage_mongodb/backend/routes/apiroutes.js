1ae41c92c057bbe75a4058f1247615dec49b3c47
Erno Hentonen <erno.hentonen@opiframe.com>
2022-05-17 09:56:28 +0300

    apiroutes to mongodb

--- react/50_shopping_app/03_stage_mongodb/backend/routes/apiroutes.js
+++ react/50_shopping_app/03_stage_mongodb/backend/routes/apiroutes.js
@@ -44,44 +44,35 @@ router.post("/shopping",function(req,res) {
 })
 
 router.delete("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id,10);
-	for(let i = 0;i<database.length;i++) {
-		if(tempId === database[i].id) {
-			if(req.session.user !== database[i].user) {
-				return res.status(409).json({message:"You are not authorized to remove this item"});
-			}
-			database.splice(i,1);
-			return res.status(200).json({message:"Success!"})
+	itemModel.deleteOne({"_id":req.params.id,"user":req.session.user},function(err) {
+		if(err) {
+			console.log("Failed to remove item. Reason:",err);
+			return res.status(500).json({message:"Internal server error"});
 		}
-	}
-	return res.status(404).json({message:"not found"});
+		return res.status(200).json({message:"success"});
+	})
 })
 
 router.put("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id,10);
 	if(!req.body) {
 		return res.status(400).json({message:"Bad request"});
 	}
 	if(!req.body.type) {
 		return res.status(400).json({message:"Bad request"});
 	}
-	let item = {
+	let item ={
 		type:req.body.type,
 		count:req.body.count,
 		price:req.body.price,
-		id:tempId,
 		user:req.session.user
 	}
-	for(let i = 0;i<database.length;i++) {
-		if(tempId === database[i].id) {
-			if(req.session.user !== database[i].user) {
-				return res.status(409).json({message:"You are not authorized to edit this item"});
-			}			
-			database.splice(i,1,item);
-			return res.status(200).json({message:"Success!"})
+	itemModel.replaceOne({"_id":req.params.id,"user":req.session.user},item,function(err) {
+		if(err) {
+			console.log("Failed to edit item. Reason:",err);
+			return res.status(500).json({message:"Internal server error"});
 		}
-	}
-	return res.status(404).json({message:"not found"});
+		return res.status(200).json({message:"success"});		
+	})
 });
 
 module.exports = router;
