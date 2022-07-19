fd800d4bd89d2fc593e7bee045d5a4a7296d735d
Erno Hentonen <erno.hentonen@opiframe.com>
2022-07-19 11:02:14 +0300

    useaction

--- react/60_typescript/05_shopping_app_basic/src/hooks/useAction.tsx
+++ react/60_typescript/05_shopping_app_basic/src/hooks/useAction.tsx
@@ -41,3 +41,84 @@ const listReducer = (state:AppState,action:Action) => {
 			return state;
 	}
 }
+
+export const useAction = ():[ShoppingItem[],boolean,() => void, (item:ShoppingItem) => void, (id:number | string) => void, (item:ShoppingItem) => void] => {
+	
+	const [urlRequest,setUrlRequest] = useState<FetchState>({
+		request:new Request("",{})
+	})
+	
+	const [state,dispatch] = useReducer(listReducer,initialState);
+	
+	useEffect(() => {
+		if(!urlRequest.request) {
+			return;
+		}
+		
+		const fetchData = async () => {
+			dispatch({type:"LOADING",payload:{}});
+			const response = await fetch(urlRequest.request);
+			dispatch({type:"STOP_LOADING",payload:{}});
+			if(!response) {
+				return;
+			}
+			if(response.ok) {
+				if(urlRequest.request.method === "GET") {
+					const data = await response.json();
+					dispatch({type:"FETCH_LIST_DONE",payload:data})
+				} else {
+					getList();
+				}
+			} else {
+				console.log("Server responded with a status:"+response.status);
+			}
+			
+		fetchData();	
+		}
+	},[urlRequest.request])
+	
+	const getList = () => {
+		let tempRequest = new Request("/api/shopping",{
+			method:"GET",
+			headers:{"Content-type":"application/json"}
+		})
+		setUrlRequest({
+			request:tempRequest
+		})
+	}
+
+
+	const addItem = (item:ShoppingItem) => {
+		let tempRequest = new Request("/api/shopping",{
+			method:"POST",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(item)
+		})
+		setUrlRequest({
+			request:tempRequest
+		})
+	}
+
+	const removeItem = (id: number | string) => {
+		let tempRequest = new Request("/api/shopping/"+id,{
+			method:"DELETE",
+			headers:{"Content-type":"application/json"}
+		})
+		setUrlRequest({
+			request:tempRequest
+		})
+	}	
+
+	const editItem = (item:ShoppingItem) => {
+		let tempRequest = new Request("/api/shopping/"+item.id,{
+			method:"PUT",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(item)
+		})
+		setUrlRequest({
+			request:tempRequest
+		})
+	}
+	
+	return [state.list,state.loading,getList,addItem,removeItem,editItem];
+}
