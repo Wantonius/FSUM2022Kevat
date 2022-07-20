edf9943ed3e0395df697bfee92cff3709bcbf49f
Erno Hentonen <erno.hentonen@opiframe.com>
2022-07-20 11:01:35 +0300

    typescript redux 2

--- react/60_typescript/06_shopping_app_with_redux/src/index.tsx
+++ react/60_typescript/06_shopping_app_with_redux/src/index.tsx
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+import {createStore,applyMiddleware,Store,AnyAction,combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {AppState} from './types/states';
+import loginReducer from './reducers/loginReducer';
+import shoppingReducer from './reducers/shoppingReducer';
+
+const rootReducer = combineReducers<AppState>({
+	login:loginReducer,
+	shopping:shoppingReducer
+})
+
+const store:Store<AppState,AnyAction> = createStore(rootReducer,applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+	<BrowserRouter>
+	<Provider store={store}>
     <App />
+	</Provider>
+	</BrowserRouter>
   </React.StrictMode>
 );
 
